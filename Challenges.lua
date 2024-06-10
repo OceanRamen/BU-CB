7c2142b5ea23ecec61df0d9d29fc644869c4db88
OceanRamen <wt557978@gmail.com>
2024-06-10 11:37:24 +0100

    feat(budgeting): add challenge Add budgeting challenge: Start with $100, no intrest, blind reward or hand money.

--- Challenges.lua
+++ Challenges.lua
@@ -10,6 +10,7 @@ function ChallengeMod.addLocalization()
   G.localization.misc.challenge_names.c_mod_jimboful_1 = "Jimboful"
   G.localization.misc.challenge_names.c_mod_swapped_pockets_1 = "Swapped Pockets"
   G.localization.misc.challenge_names.c_mod_student_loan_debt_1 = "Student Loan Debt"
+  G.localization.misc.challenge_names.c_mod_budgeting_1 = "Budgeting"
   --  Challenge Descriptions
   G.localization.misc.v_text.ch_c_no_shop_planets = { "Planets no longer appear in the {C:attention}shop" }
   G.localization.misc.v_text.ch_c_no_shop_tarots = { "Tarot cards no longer appear in the {C:attention}shop" }
