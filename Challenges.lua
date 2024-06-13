384cce74af692e2ec678b99b800ca9e593852a9f
OceanRamen <wt557978@gmail.com>
2024-06-13 23:43:50 +0100

    chore(Challenge Names): automated the name localization process

--- Challenges.lua
+++ Challenges.lua
@@ -1,23 +1,35 @@
 local lovely = require("lovely")
 local nativefs = require("nativefs")
 
+local function get_chal_files(directory)
+  local files = {}
+  local items = nativefs.getDirectoryItems(directory)
+  for _, item in ipairs(items) do
+    if item:match("%.lua$") then
+      table.insert(files, item)
+    end
+  end
+  return files
+end
+
+local directory = lovely.mod_dir .. "/ChallengeMod/Challenges/"
+
+local lua_files = get_chal_files(directory)
+
+local CustomChallenges = {}
+
+for _, file in ipairs(lua_files) do
+  local file_path = directory .. "/" .. file
+  local variable_name = file:gsub("%.lua$", "")
+  CustomChallenges[variable_name] = dofile(file_path)
+end
+
 function ChallengeMod.addLocalization()
-  --  Challenge Names
-  G.localization.misc.challenge_names.c_mod_budgeting_1 = "Budgeting"
-  G.localization.misc.challenge_names.c_mod_bullseye_1 = "Bullseye"
-  G.localization.misc.challenge_names.c_mod_fleeting_1 = "Fleeting Memory"
-  G.localization.misc.challenge_names.c_mod_jimboful_1 = "Jimboful"
-  G.localization.misc.challenge_names.c_mod_load_bearing_1 = "Load Bearing"
-  G.localization.misc.challenge_names.c_mod_riffraff_1 = "Riff-Raffle"
-  G.localization.misc.challenge_names.c_mod_student_loan_debt_1 = "Student Loan Debt"
-  G.localization.misc.challenge_names.c_mod_swapped_pockets_1 = "Swapped Pockets"
-  G.localization.misc.challenge_names.c_mod_tarot_tycoon_1 = "Tarot Tycoon"
-  G.localization.misc.challenge_names.c_mod_unfortunate_1 = "Unfortunate"
-  G.localization.misc.challenge_names.c_mod_examination_1 = "Examination"
-  G.localization.misc.challenge_names.c_mod_payasyougo_1 = "Pay As You Go"
+  for i, v in pairs(CustomChallenges) do
+    G.localization.misc.challenge_names[v.id] = v.name
+  end
+
   --  Challenge Descriptions
-  G.localization.misc.v_text.ch_c_gold_stake = { "Playing on the {C:attention}Gold Stake" }
-  G.localization.misc.v_text.ch_c_exam_stake = { "Playing on the {C:attention}Examination Stake" }
   G.localization.misc.v_text.ch_c_all_perishable = { "All Jokers are {C:perishable}Perishable{}" }
   G.localization.misc.v_text.ch_c_all_rental = { "All Jokers are {C:rental}Rental{}" }
   G.localization.misc.v_text.ch_c_cm_force_hand = { "Only #1#{}s will score" }
@@ -68,29 +80,6 @@ function Blind:defeat(silent)
     G.STATE_COMPLETE = false
     G.SETTINGS.paused = false
   end
-end
-
-local function get_chal_files(directory)
-  local files = {}
-  local items = nativefs.getDirectoryItems(directory)
-  for _, item in ipairs(items) do
-    if item:match("%.lua$") then
-      table.insert(files, item)
-    end
-  end
-  return files
-end
-
-local directory = lovely.mod_dir .. "/ChallengeMod/Challenges/"
-
-local lua_files = get_chal_files(directory)
-
-local CustomChallenges = {}
-
-for _, file in ipairs(lua_files) do
-  local file_path = directory .. "/" .. file
-  local variable_name = file:gsub("%.lua$", "")
-  CustomChallenges[variable_name] = dofile(file_path)
 end
 
 for i, v in pairs(CustomChallenges) do
