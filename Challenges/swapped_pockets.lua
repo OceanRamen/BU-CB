b6d65a119c3750bcdea56adadd1bc7ec5d9b0186
surskitt <surskitt@users.noreply.github.com>
2024-06-14 08:42:41 +0100

    fix(swapped_pockets): fix name in challenge file

--- Challenges/swapped_pockets.lua
+++ Challenges/swapped_pockets.lua
@@ -1,41 +1,41 @@
 return {
-	name = "Swapepd Pockets",
-	id = "c_mod_swapped_pockets_1",
-	rules = {
-		modifiers = {
-			{
-				id = "joker_slots",
-				value = 2,
-			},
-			{
-				id = "hand_size",
-				value = 5,
-			},
-			{
-				id = "consumable_slots",
-				value = 8,
-			},
-			{
-				id = "hands",
-				value = "3",
-			},
-			{
-				id = "discards",
-				value = "4",
-			},
-		},
-		custom = {
-			{
-				id = "cm_credit",
-				value = "CampfireCollective",
-			},
-		},
-	},
-	jokers = {},
-	consumeables = {},
-	vouchers = {},
-	deck = {
-		type = "Challenge Deck",
-	},
-	restrictions = {},
+  name = "Swapped Pockets",
+  id = "c_mod_swapped_pockets_1",
+  rules = {
+    modifiers = {
+      {
+        id = "joker_slots",
+        value = 2,
+      },
+      {
+        id = "hand_size",
+        value = 5,
+      },
+      {
+        id = "consumable_slots",
+        value = 8,
+      },
+      {
+        id = "hands",
+        value = "3",
+      },
+      {
+        id = "discards",
+        value = "4",
+      },
+    },
+    custom = {
+      {
+        id = "cm_credit",
+        value = "CampfireCollective",
+      },
+    },
+  },
+  jokers = {},
+  consumeables = {},
+  vouchers = {},
+  deck = {
+    type = "Challenge Deck",
+  },
+  restrictions = {},
 }
