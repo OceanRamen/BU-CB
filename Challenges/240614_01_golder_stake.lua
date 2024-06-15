f3ad508efc31e1117926f1956710ed1616aeb03f
surskitt <surskitt@users.noreply.github.com>
2024-06-15 16:11:28 +0100

    fix(golder): rename challenges to order properly

--- Challenges/240614_01_golder_stake.lua
+++ Challenges/240614_01_golder_stake.lua
@@ -1,6 +1,6 @@
 return {
-  name = "Even Golder Stake",
-  id = "c_mod_evengolderstake_1",
+  name = "Golder Stake",
+  id = "c_mod_golderstake_1",
   rules = {
     custom = {
       {
@@ -18,14 +18,12 @@ return {
       },
       { id = "enable_rentals_in_shop" },
       { id = "enable_eternals_in_shop" },
-      { id = "enable_perishables_in_shop"},
-      { id = 'no_reward_specific', value = 'Small'}
+      { id = "enable_perishables_in_shop" },
+      { id = "no_reward_specific", value = "Small" },
     },
     modifiers = {
-      { id = 'discards', value = 2},
-      { id = 'hands', value = 3},
-      {id = 'hand_size', value = 7},
-    }
+      { id = "discards", value = 2 },
+    },
   },
   deck = {
     type = "Abandoned Deck",
--- Challenges/240614_02_even_golder_stake.lua
+++ Challenges/240614_02_even_golder_stake.lua
@@ -1,6 +1,6 @@
 return {
-  name = "Golder Stake",
-  id = "c_mod_golderstake_1",
+  name = "Even Golder Stake",
+  id = "c_mod_evengolderstake_1",
   rules = {
     custom = {
       {
@@ -18,12 +18,14 @@ return {
       },
       { id = "enable_rentals_in_shop" },
       { id = "enable_eternals_in_shop" },
-      { id = "enable_perishables_in_shop"},
-      { id = 'no_reward_specific', value = 'Small'}
+      { id = "enable_perishables_in_shop" },
+      { id = "no_reward_specific", value = "Small" },
     },
     modifiers = {
-      { id = 'discards', value = 2},
-    }
+      { id = "discards", value = 2 },
+      { id = "hands", value = 3 },
+      { id = "hand_size", value = 7 },
+    },
   },
   deck = {
     type = "Abandoned Deck",
