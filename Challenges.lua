6b36311c2b5b8bfc6ca5fabe8e506c90504a1ad6
OceanRamen <wt557978@gmail.com>
2024-06-16 21:53:33 +0100

    Feat(cm_noshop): Added "No shop" custom rule. If true, shop is bypassed and players go straight to blind_select screen.

--- Challenges.lua
+++ Challenges.lua
@@ -39,35 +39,10 @@ function ChallengeMod.addLocalization()
   G.localization.misc.v_text.ch_c_cm_stake = { "Playing on the {C:attention}#2#{} stake" }
   G.localization.misc.v_text.ch_c_cm_scaling = { "Custom ante scaling" }
   G.localization.misc.v_text.ch_c_cm_scaling_manual = { "Custom ante and blind scaling" }
+  G.localization.misc.v_text.ch_c_cm_noshop = { "{C:attention}No Shop" }
   --  Credit Tags
   G.localization.misc.v_text.ch_c_cm_credit = { "Concept by: {C:green}#1#{}" }
 end
-
--- function ChallengeMod.custom_stake(stake)
---   G.GAME.stake = stake
---   if G.GAME.stake >= 2 then
---     G.GAME.modifiers.no_blind_reward = G.GAME.modifiers.no_blind_reward or {}
---     G.GAME.modifiers.no_blind_reward.Small = true
---   end
---   if G.GAME.stake >= 3 then
---     G.GAME.modifiers.scaling = 2
---   end
---   if G.GAME.stake >= 4 then
---     G.GAME.modifiers.enable_eternals_in_shop = true
---   end
---   if G.GAME.stake >= 5 then
---     G.GAME.starting_params.discards = G.GAME.starting_params.discards - 1
---   end
---   if G.GAME.stake >= 6 then
---     G.GAME.modifiers.scaling = 3
---   end
---   if G.GAME.stake >= 7 then
---     G.GAME.modifiers.enable_perishables_in_shop = true
---   end
---   if G.GAME.stake >= 8 then
---     G.GAME.modifiers.enable_rentals_in_shop = true
---   end
--- end
 
 function get_blind_amount(ante)
   local k = 0.75
@@ -185,7 +160,6 @@ function taxedAlert(text)
   }))
 end
 
-
 function Card:set_perishable(_perishable)
   self.ability.perishable = nil
   if (self.config.center.perishable_compat or G.GAME.modifiers.all_perishable) and not self.ability.eternal then
@@ -209,7 +183,7 @@ local blind_defeat_ref = Blind.defeat
 function Blind:defeat(silent)
   if G.GAME.modifiers.cm_negative_interest then
     local tax = math.min(math.floor(G.GAME.dollars / 5), G.GAME.interest_cap / 5)
-    taxedAlert("-$"..tax)
+    taxedAlert("-$" .. tax)
     ease_dollars(-tax)
   end
 
