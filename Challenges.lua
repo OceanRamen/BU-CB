b9ed93d8cd2be488e6fe924cbb6f3fe59cd9ae50
surskitt <surskitt@users.noreply.github.com>
2024-06-22 20:38:48 +0100

    feat: add automatic info rule for non challenge deck

--- Challenges.lua
+++ Challenges.lua
@@ -44,6 +44,7 @@ function ChallengeMod.addLocalization()
   G.localization.misc.v_text.ch_c_cm_hand_kills = { "Lose the game if played hand contains a {C:blue}#1#{}" }
   G.localization.misc.v_text.ch_c_cm_all_facedown = { "All cards except those held in hand are face down" }
   G.localization.misc.v_text.ch_c_cm_mult_dollar_cap = { "{C:blue}Mult{} cannot exceed the current {C:money}$" }
+  G.localization.misc.v_text.ch_c_cm_deck = { "Played on #1#{}" }
   --  Credit Tags
   G.localization.misc.v_text.ch_c_cm_credit = { "Concept by: {C:green}#1#{}" }
 end
@@ -179,6 +180,10 @@ function mod_mult(_mult)
   return _mult
 end
 
-for i, v in pairs(CustomChallenges) do
+for _, v in pairs(CustomChallenges) do
+  if v.deck.type ~= "Challenge Deck" then
+    table.insert(v.rules.custom, 1, { id = "cm_deck", value = v.deck.type })
+  end
+
   table.insert(G.CHALLENGES, #G.CHALLENGES + 1, v)
 end
